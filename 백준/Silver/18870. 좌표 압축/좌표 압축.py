4cbab385d60b2928913ceeb8f600d618f651f314
yeoyeong <132591173+yeoyeong@users.noreply.github.com>
2024-07-31 17:02:48 +0900

    [Silver II] Title: 좌표 압축, Time: 1832 ms, Memory: 144340 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,22 @@
+# 좌표 압축
+# 0부터 작은 숫자를 저장
+
+import sys
+
+# 입력 받기
+n = int(input())
+coords = list(map(int, input().split()))
+
+
+# 중복 제거 및 정렬
+sorted_unique_coords = sorted(set(coords))
+
+# 좌표값을 딕셔너리에 저장
+coord_dict = {coord: idx for idx, coord in enumerate(sorted_unique_coords)}
+
+# 압축된 좌표 출력
+# compressed_coords = [coord_dict[coord] for coord in coords]
+# print(' '.join(map(str, compressed_coords)))
+
+for coord in coords:
+    print(coord_dict[coord], end=" ")
