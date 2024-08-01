33b4703d278274c2ddb26a9ff6f158447be66f22
yeoyeong <132591173+yeoyeong@users.noreply.github.com>
2024-08-01 20:17:38 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
@@ -1,50 +1,50 @@
-#내코드x
 import sys
+sys.setrecursionlimit(1000000)
 
 input = sys.stdin.readline
 n = int(input().strip())
 
 data = [input().strip() for _ in range(n)]
 
-# 데이터를 좌표로 변환
+#데이터를 좌표로 변환
 matrix = [list(map(int, row.split())) for row in data]
 
 WIN_MESSAGE = "HaruHaru"
 LOSE_MESSAGE = "Hing"
 
-# DFS를 반복문으로 구현
-def iterative_dfs(start_x, start_y, grid, n):
-    stack = [(start_x, start_y)]
-    visited = set()
 
-    while stack:
-        x, y = stack.pop()
+# 이동 가능 방향 만큼 dfs를 실행 했을 때
+# -1이면 성공
+# 장외면 패배
 
-        if (x, y) in visited:
-            continue
 
-        visited.add((x, y))
-
-        if grid[x][y] == -1:
+#dfs
+def dfs(x, y, grid, n, m):
+    if grid[x][y] == -1:
             return True
+    if grid[x][y] == 0:
+            return False
+    move = grid[x][y]
+    directions = [(move, 0), (0, move)]
 
-        move = grid[x][y]
-        directions = [(move, 0), (0, move)]
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy
 
-        for dx, dy in directions:
-            nx, ny = x + dx, y + dy
-
-            if 0 <= nx < n and 0 <= ny < n:
-                stack.append((nx, ny))
+        if 0 <= nx < n and 0 <= ny < m :
+            if dfs(nx, ny, grid, n, m):
+                return True
 
     return False
 
-# 게임 결과 확인
-def count_grass_areas(grid):
-    if iterative_dfs(0, 0, grid, n):
-        return WIN_MESSAGE
-    else:
-        return LOSE_MESSAGE
 
+#중복 발생 가능성이 없기에 방문 체크는 제외했다
+def count_grass_areas(grid):
+    if dfs(0, 0, grid, n, n):
+         return WIN_MESSAGE
+    else:
+         return LOSE_MESSAGE
+         
+    
 result = count_grass_areas(matrix)
+
 print(result)
